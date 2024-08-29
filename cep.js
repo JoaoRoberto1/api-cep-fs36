76e0ebd764d912169bcc08186f8ab6bf56e9060b
Joao Roberto De Oliveira Felix <0000000025@al.sesi-ce.org.br>
2024-08-29 20:43:29 -0300

    Subindo um resumão de objetos

--- cep.js
+++ cep.js
@@ -1,39 +1,3 @@
-/*
-document.querySelector('#cep-input').addEventListener('input', function () {
-    const cep = this.value.replace(/\D/g, ''); 
-    const estado = document.querySelector('#estado');
-    const cidade = document.querySelector('#cidade');
-    const bairro = document.querySelector('#bairro');
-    const rua = document.querySelector('#rua');
-
-
-    if (cep.length === 8) {
-        axios.get(`https://brasilapi.com.br/api/cep/v2/${cep}`)
-            .then(response => {
-                const data = response.data;
-                estado.value = data.state || '';
-                cidade.value = data.city || '';
-                bairro.value = data.neighborhood || '';
-                rua.value = data.street || '';
-            })
-            .catch(error => {
-                console.error('Erro ao buscar o CEP:', error);
-                alert('CEP NÃO ENCONTRADO');
-  
-                estado.value = '';
-                cidade.value = '';
-                bairro.value = '';
-                rua.value = '';
-            });
-    } else {
-        estado.value = '';
-        cidade.value = '';
-        bairro.value = '';
-        rua.value = '';
-    }
-});
-*/
-
 document.querySelector('#cep-input').addEventListener('input', async (event) => {
     const cep = event.target.value.replace(/\D/g, ''); 
     const estado = document.querySelector('#estado');
